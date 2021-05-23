5f7385d8d94091d89831b59a4f105bac60df423a
NAVOMITA GHOSHAL <64421634+NAV0MITA11GHOSHAL@users.noreply.github.com>
2021-05-23 09:25:19 +0530

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,11 @@
+let navElements = document.querySelectorAll(".navEl"),
+    currentNav = 0;
+
+/* Change active class of navBar */
+navElements.forEach((a) => {
+    a.addEventListener("click", () => {
+        navElements[currentNav].classList.remove("active");
+        a.classList.add("active");
+        currentNav = a.dataset.pos;
+    });
+});
